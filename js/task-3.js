67ad72ca534e07f18650ac46fe1119922d352678
EvgenS-it <evgens.it101@gmail.com>
2024-03-03 21:53:23 +0200

    task-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,7 @@
 function checkForSpam(message) {
-    
+    message = message.toLowerCase();
+
+    return (message.includes("spam") || message.includes("sale"));
 }
 
 console.log(checkForSpam("Latest technology news")); // false
